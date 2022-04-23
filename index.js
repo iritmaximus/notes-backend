fda3a92b5038f99e90951e120efdd45c4f76a3a0
iritmaximus <aaro.yliayho@gmail.com>
2022-04-23 10:55:34 +0300

    did something

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const app = require("./app");
+const http = require("http");
+const config = require(".utils/config");
+const logger = require(".utils/logger");
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  logger.info(`server running in port ${config.PORT}`)
+});
+
+
+
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const Note = require("./models/note")
+const Note = require("./models/note");
 
 app.use(express.json());
 
@@ -25,88 +38,87 @@ let notes = [
     date: "2020-01-10T19:20:14.298Z",
     important: true
   }
-]
+];
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", (req, res) => {
   Note.find({}).then(notes => {
     res.json(notes);
-  })
-})
+  });
+});
 
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing"
+    });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  })
+  });
 
   note.save()
     .then(savedNote => {
       response.json(savedNote);
     })
     .catch(error => next(error));
-})
+});
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note);
-  })
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      response.json(note);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-app.put('/api/notes/:id', (request, response, next) => {
+app.put("/api/notes/:id", (request, response, next) => {
   const { content, important } = request.body;
 
   Note.findByIdAndUpdate(
-    request.params.id, 
-    { content, important },   
-    { new: true, runValidators: true, context: 'query' }  
-  ); 
-   .then(updatedNote => {
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then(updatedNote => {
       response.json(updatedNote);
-    });
+    })
     .catch(error => next(error));
-})
+});
 
 
-const errorHandler (error, req, res, next) => {
+const errorHandler = (error, req, res, next) => {
   console.error(error.message);
-  
-  if (error.name == "CastError") {
-    return res.status(400).send({ error: "malformatted id" })
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message })
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
-}
+};
 
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ utils/logger.js
@@ -0,0 +1,13 @@
+// separate logger module
+
+const info = (...params) => {
+  console.log(...params);
+};
+
+const error = (...params) => {
+  console.error(...params);
+};
+
+module.exports = {
+  info, error
+};
